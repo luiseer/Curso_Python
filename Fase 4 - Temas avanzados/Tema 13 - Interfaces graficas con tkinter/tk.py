48a6921f6c842130493c06e4088b7b5f10bc0d73
luiseer <85654418+luiseer@users.noreply.github.com>
2024-01-27 08:34:36 -0600

    interfases graficas con python

---
+++ Fase 4 - Temas avanzados/Tema 13 - Interfaces graficas con tkinter/tk.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import *
+from tkinter import messagebox
+
+from tkinter import *
+
+root = Tk()
+root.title("Hola Mundo")
+root.iconbitmap("@hola.ico")
+root.resizable(False, False)
+root.mainloop()
