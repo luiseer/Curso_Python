cb9b3ae34b0601ad511264e8553189849219cf38
luiseer <85654418+luiseer@users.noreply.github.com>
2024-02-02 11:41:42 -0600

    interfases graficas

---
+++ Fase 4 - Temas avanzados/Tema 13 - Interfaces graficas con tkinter/frame.py
@@ -0,0 +1,22 @@
+from tkinter import *
+root = Tk()
+
+root.title("hola mundo")
+root.iconbitmap('hola.ico')
+
+frame = Frame(root, width=400, height=380)
+
+frame.pack(side='right')
+
+frame.config(cursor="pirate")
+frame.config(bg='lightblue')
+frame.config(bd=25)
+frame.config(relief='sunken')
+
+root.config(cursor="pirate")
+root.config(bg='blue')
+root.config(bd=35)
+root.config(relief='ridge')
+
+
+root.mainloop()
--- Fase 4 - Temas avanzados/Tema 13 - Interfaces graficas con tkinter/tk.py
+++ Fase 4 - Temas avanzados/Tema 13 - Interfaces graficas con tkinter/tk.py
@@ -1,12 +1,8 @@
-import tkinter as tk
-import tkinter.ttk as ttk
 from tkinter import *
-from tkinter import messagebox
-
-from tkinter import *
-
 root = Tk()
-root.title("Hola Mundo")
-root.iconbitmap("@hola.ico")
-root.resizable(False, False)
+
+root.title("hola mundo")
+root.iconbitmap('hola.ico')
+root.resizable(True, False)
+
 root.mainloop()
